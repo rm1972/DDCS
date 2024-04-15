f4916b35d33b6bafe2df81e93ecdbe2dcc43b292
rm1972 <81978346+rm1972@users.noreply.github.com>
2024-04-15 21:00:24 +0800

    Update get_variance.py

--- get_variance.py
+++ get_variance.py
@@ -3,13 +3,13 @@ import torch
 
 mean_path = "ID_prototype/mean.npy"
 
-class_means_dict = np.load(mean_path, allow_pickle=True).item()  # 加载为字典
+class_means_dict = np.load(mean_path, allow_pickle=True).item()  
 
 class_means_list = [class_means_dict[label] for label in sorted(class_means_dict.keys())] 
 
 class_means_tensor = torch.tensor(class_means_list, dtype=torch.float32) 
 
-channel_variances = torch.var(class_means_tensor, dim=0) ##2048维
+channel_variances = torch.var(class_means_tensor, dim=0)
 
 print("Class-wise channel variances:", channel_variances)
 print(channel_variances.shape)
