7484d10d32428325ba354f72775939cd6bfb1767
rm1972 <81978346+rm1972@users.noreply.github.com>
2024-04-19 18:38:08 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,7 +1,1 @@
-# for a in $(seq 0 0.1 1.0); do
-#       for k in $(seq 850 50 1250); do
-#          python eval.py  --a $a --k $k
-#        done
-# done
-
 python eval.py --threshold_h $threshold_h --threshold_l $threshold_l  --a $a --k $k  
